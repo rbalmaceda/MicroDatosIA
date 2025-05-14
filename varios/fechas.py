9ffd5efe3049cd60645179c0c7ed456920954d82
fernanqv <valvanuz.fernandez@unican.es>
2025-05-14 13:20:38 +0200

    fechas.py como ejemplo de debugging

---
+++ varios/fechas.py
@@ -0,0 +1,20 @@
+from datetime import datetime
+
+def dias_entre_fechas(f1, f2):
+    formato = "%d/%m/%Y"
+    fecha1 = datetime.strptime(f1, formato)
+    fecha2 = datetime.strptime(f2, formato)
+    diferencia = abs((fecha2 - fecha1).days)
+    return diferencia
+
+def main():
+    f1 = "28/04/2023"
+    f2 = "02/05/2024"
+    f3 = "15/08/2023"
+    dias1 = dias_entre_fechas(f1, f2)
+    dias2 = dias_entre_fechas(f1, f3)
+    print(f"Días entre {f1} y {f2}: {dias1} ")
+    print(f"Días entre {f1} y {f3}: {dias2} ")
+
+if __name__ == "__main__":
+    main()
